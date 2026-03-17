08e383d8b05013df9966b130dd40134a346b6197
Aditya Dubey <uhai3582@gmail.com>
2026-03-17 10:02:32 +0530

    Challenge given by gpt

---
+++ funproblem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+
+    cout<<"Enter Your Name: ";
+    cin >> name;
+
+    cout<<"Hello"<<name<<endl;
+    cout<<"Welcome to the world of AI Programming 🤖";
+    return 0;
+}
