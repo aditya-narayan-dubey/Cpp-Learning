60ac79a6d76303782675ac38c93ad0c4f38eb591
Aditya Dubey <uhai3582@gmail.com>
2026-03-18 12:07:03 +0530

    Add files via upload

---
+++ challenge1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    string city;
+    string DreamJob;
+
+    cout<<"Enter your name: ";
+    cin>> name;
+
+    cout<<"Enter your city: ";
+    cin>> city;
+
+    cout<<"Enter your DreamJob: ";
+    cin>> DreamJob;
+
+    cout <<"Hello "<< name <<" from "<< city <<endl;
+    cout<<"Future AI Engineer Detected";
+    return 0;
+
+}
