4d192540f197eac3fb1ef39e626985d212cdd5ba
Aditya Dubey <uhai3582@gmail.com>
2026-03-18 12:09:29 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int guess;
+    int seceret=5;
+
+    cout<<"Guess the number nigga: ";
+    cin>>guess;
+
+    while(guess != seceret){
+        cout<<"Try again Nigga u r noob as hell: ";
+        cin>>guess;
+    }
+    cout<<"Nigga u did it 🐷🐷";
+    return 0;
+}
