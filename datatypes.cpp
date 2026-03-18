5b5e4df8d488b70f4ca71f51fea6b734d43bb818
Aditya Dubey <uhai3582@gmail.com>
2026-03-18 12:09:02 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // Integer data type
+    int x=10;
+    int y=20;
+    int z=30;
+
+
+    // double data type= number including decimal values
+    double price= 10.99;
+    double gpa=2.5;
+    double temperature= 25.1;
+
+    // Character data type= single character only
+    char grade= 'A';
+    char initial= 'C';
+    char dollarsign= '$';
+
+
+    // boolean data type= true or false values
+    bool student= true;
+    bool isRaining= false;
+    bool gameOver= false;
+
+    // string data type= objects that represent a sequence of text
+    string name= "Aditya";
+    string food= "Pizza";
+    string address= "123 Main St";
+    cout<< "Hello " << name<< endl;
+    cout<< grade<< endl;
+}
