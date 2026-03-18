9e7437cf0a2c7fadc5c01882b182c51963fd0a3b
Aditya Dubey <uhai3582@gmail.com>
2026-03-18 12:08:16 +0530

    Add files via upload

---
+++ ifelsecondition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+
+    cout<<"Enter your age: ";
+    cin>> age;
+
+    if(age >= 18){
+        cout<<"You can Vote";
+    }
+    else{
+        cout<<"You cannot Vote";
+    }
+    return 0;
+}
