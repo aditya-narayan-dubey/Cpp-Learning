bcbd63e990b8cfdcfb94b27e7ee1adcf9dec25a3
Aditya Dubey <uhai3582@gmail.com>
2026-03-19 11:08:54 +0530

    Add files via upload

---
+++ aiguessthenumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+int guess;
+int secret=65;
+int attempts=0;
+
+cout<<"Go ahead Choose ur number Nigga: ";
+cin>>guess;
+
+while(guess != secret)
+{
+    attempts;
+
+if(guess > secret){
+    cout<<"Nigga u are thinking too high number choose a lower one🤪🤪: ";
+}
+else{
+    cout<<"Nigga u are thinking too low number choose a higher one🤪🤪: ";
+}
+cin>>guess;}
+attempts++;
+
+cout<<"Nigga u did it😒😒😒😒😒😒😒😒😒😒😒😒😒"<<endl;
+cout<<"Nigga u took "<<attempts<<" attempt u r clearly noob🤣🤣🤣🤣🤣🤣🤣🤣";
+return 0;
+}
