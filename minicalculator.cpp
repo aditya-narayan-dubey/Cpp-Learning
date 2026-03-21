b92c05401899bc7cb37bf9964d27252b19348a53
Aditya Dubey <uhai3582@gmail.com>
2026-03-21 16:47:17 +0530

    Add files via upload

---
+++ minicalculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+
+    cout<<"Enter your first number: ";
+    cin>> num1;
+
+    cout<<"Enter your second number: ";
+    cin>> num2;
+
+    cout<<"Addition: "<<num1 + num2<<endl;
+    cout<<"Subtraction: "<<num1 - num2<<endl;
+    cout<<"Mutiplication: "<<num1 * num2<<endl;
+    cout<<"Division: "<<num1 / num2<<endl;
+    cout<<"Modulus: "<<num1 % num2<<endl;
+    return 0;
+}
