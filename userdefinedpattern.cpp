64ed5b151f31fde625a87cd56a2a858e724b8146
Aditya Dubey <uhai3582@gmail.com>
2026-03-25 18:24:07 +0530

    Add files via upload

---
+++ userdefinedpattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"Enter your number: ";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*Output:
+Enter your number: 7
+*
+**
+***
+****
+*****
+******
+*******      */
