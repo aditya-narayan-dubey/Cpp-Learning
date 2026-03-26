b0e1689415fee909f4da137dc4b468ebcb154b05
Aditya Dubey <uhai3582@gmail.com>
2026-03-26 12:25:35 +0530

    Add files via upload

---
+++ challenge9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n = 5;
+
+    for (int i = 0; i < n; i++){
+        for (int j = n; j > i; j--){
+            cout<<" ";
+        }
+        for(int k = 0; k <= i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+        }
+        
+    
+
+    }
+    /**
+     **
+    ***
+   ****
+  *****    */
