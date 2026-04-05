a58c6f5d2efe9b209d4c2e83b0c26293a163329b
Aditya Dubey <uhai3582@gmail.com>
2026-04-05 22:31:17 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3] = {10, 20, 30};
+
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+
+    return 0;
+}
