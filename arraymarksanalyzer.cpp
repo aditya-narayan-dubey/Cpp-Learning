c0b6babad88b440bc323d67dcc9b9966b6f6e052
Aditya Dubey <uhai3582@gmail.com>
2026-04-07 20:52:18 +0530

    Add files via upload

---
+++ arraymarksanalyzer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    float sum=0;
+
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<5;i++){
+        sum += arr[i];
+    }
+    cout<<"Average is: "<<sum/5;
+}
