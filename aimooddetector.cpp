cfd938b3e0360c7971521151abe6fcc7012adf85
Aditya Dubey <uhai3582@gmail.com>
2026-04-24 11:58:10 +0530

    Update aimooddetector.cpp

--- aimooddetector.cpp
+++ aimooddetector.cpp
@@ -8,13 +8,13 @@ int main()
     cin>> mood;
 
     if(mood == 1){
-        cout<<"keep smiling and be happy 😊 just like Aditri keeps smiling";
+        cout<<"keep smiling and be happy 😊 just like ME keeps smiling";
     }
     else if(mood == 2){
-        cout<<"dont feel sad watch Aditri's face and be happy";
+        cout<<"dont feel sad watch Breaking Bad and be happy";
     }
     else if(mood == 3){
-        cout<<"Take a deep breath and troll Aditri u will be Happy 😎";
+        cout<<"Take a deep breath and troll gpt u will be Happy 😎";
     }
     else{
         cout<<"Invalid number";
